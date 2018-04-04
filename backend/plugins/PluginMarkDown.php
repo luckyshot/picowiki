83adb533206755a2127f5e29b0ec2fee657a5c76
Xavi <xavi@xaviesteve.com>
2018-04-04 23:24:19 +0200

    Renamed PluginParsedown to PluginMarkDown

--- backend/plugins/PluginMarkDown.php
+++ backend/plugins/PluginMarkDown.php
@@ -4,13 +4,13 @@ PicoWiki MarkDown
 This plugin converts Markdown format to HTML
 
 */
-Class PluginParseDown
+Class PluginMarkDown
 {
 	static $version = '1.0.0';
 
 	static function run( $PicoWiki ){
 		$PicoWiki->event('view_after', NULL, function($PicoWiki) {
-            require_once __DIR__ . '/PluginParseDown/Parsedown.php';
+            require_once __DIR__ . '/PluginMarkDown/Parsedown.php';
             $Parsedown = new Parsedown();
 	        return $Parsedown->text($PicoWiki->html);
 		});
