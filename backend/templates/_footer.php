86d83525e8578c6b753a92d1bf3338e9a9a56fad
Xavi <xavi@xaviesteve.com>
2019-10-24 14:35:58 +0200

    Made Sidebar menu a toggleable hamburger

--- backend/templates/_footer.php
+++ backend/templates/_footer.php
@@ -1,6 +1,8 @@
 </div>
 
 <nav id="nav-main" class="nav-main">
+    <input type="checkbox" id="nav-checkbox">
+    <label for="nav-checkbox" id="nav-toggle">☰</label>
     <ul>
         <?php foreach( $this->file_list as $file ){ ?>
             <li><a href="<?=$this->config['app_url'].$file?>"><?=$file?></a></li>
